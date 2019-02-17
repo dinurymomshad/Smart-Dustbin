8f852286a23e0209d637244bb222e3396a4a36da
Aonmoy <47689887+MrPengueeen@users.noreply.github.com>
2019-02-17 16:16:41 +0600

    Prototype Arduino program for the Smart-Dustbin.
    Everything seems to work on a basic level. Set the interruption distance at 20 Centimeters and less for comfortable experimentation. 
    
    Issues to resolve ;
    1. Detecting objects with an angular approach to the sensor.
    2. Shrinkifying the whole setup from breadboard to PCB.
    3. Assembling the whole thing into a fully functioning actual Dustbin.

---
+++ SmartDustbinPrototype.ino
@@ -0,0 +1,32 @@
+#include <Servo.h>                 //Includes Servo library//
+int trigPin = 9;                   //Sets digital pin 9 as the Sensor's trigger pin//
+int echoPin = 7;                             //Sets disital pin 7 as the Sensor's echo pin//
+float distance, duration;                    //Declares variables//
+Servo servo1;
+void setup() {
+  Serial.begin(9600);                        //Starts serial communication//
+  servo1.attach(10);                         //Uses digital pin 10 as the signal pin for Servo Motor//
+  pinMode(trigPin, OUTPUT);                  //Sets trigPin as an output//
+  pinMode(echoPin, INPUT);                   //Sets echoPin as an input//
+}
+
+void loop() {
+  servo1.write(0);                           // Initially keeps the servo at 0 degree position//
+
+  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);         //Takes the time taken by the sensor to detect any object and puts it as the value for duration//
+
+  distance = (duration * 0.034 / 2);         //Calculates distance using the duration and velocity of sound wave//
+  if (distance <= 20) {
+    servo1.write(70);                        //If there's any interruption within 20 Centimeters of the sensor the servo opens the lid//
+    Serial.println("The distance is - ");
+    Serial.print(distance);
+    Serial.println(" Centimeter(s)");        //Prints the distance of the object//
+    delay(5000);
+    servo1.write(0);
+  }
+}
